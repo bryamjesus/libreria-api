b02c75c405383fc13a74fbea2d460490c5849ded
Bryam Talledo <bryamtalledog@gmail.com>
2022-09-26 20:33:05 -0500

    Routes

---
+++ src/routes/generoRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const controlador = require('../src/controllers/generoController');
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+  controlador.listar(req, res)
+})
+
+router.post('/',(req, res) => {
+  controlador.guardar(req, res)
+})
+
+module.exports = router;
